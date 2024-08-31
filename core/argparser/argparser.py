ac6059ded60852f30e619462cc32b7bff8280ff6
Z3_N1X <iliya.souri7890@gmail.com>
2024-08-31 18:52:50 +0330

    custom arg parser

--- core/argparser/argparser.py
+++ core/argparser/argparser.py
@@ -10,9 +10,6 @@ Functions:
     parse_args: Parse command-line arguments and return a namespace object.
 """
 import argparse
-from argparse import _FormatterClass, ArgumentParser
-from collections.abc import Sequence
-from typing import Any
 
 class ArgParser(argparse.ArgumentParser):
     """
@@ -24,40 +21,27 @@ class ArgParser(argparse.ArgumentParser):
     Methods:
         _add_args: Adds arguments to the parser.
     """
-    def __init__(self,
-                 prog: str | None = None,
-                 usage: str | None = None,
-                 description: str | None = None,
-                 epilog: str | None = None,
-                 parents: Sequence[ArgumentParser] = ...,
-                 formatter_class: _FormatterClass = ...,
-                 prefix_chars: str = "-",
-                 fromfile_prefix_chars: str | None = None,
-                 argument_default: Any = None,
-                 conflict_handler: str = "error",
-                 add_help: bool = True,
-                 allow_abbrev: bool = True,
-                 exit_on_error: bool = True
-                ) -> None:
+    def __init__(self) -> None:
 
         prog = "PCI"
         description = "Python CLI Editor"
         epilog = f"Use {prog.lower()} -help"
 
-        super().__init__(prog,
-                         usage,
-                         description,
-                         epilog,
-                         parents,
-                         formatter_class,
-                         prefix_chars,
-                         fromfile_prefix_chars,
-                         argument_default,
-                         conflict_handler,
-                         add_help,
-                         allow_abbrev,
-                         exit_on_error
+        self._add_args()
+
+        super().__init__(prog=prog,
+                         description=description,
+                         epilog=epilog,
                         )
 
     def _add_args(self):
         self.add_argument("filename")
+
+    @property
+    def filename(self):
+        """filename property.
+
+        Returns:
+            _type_: _description_
+        """
+        return self.parse_args().filename
--- core/core.py
+++ core/core.py
@@ -1,10 +1,10 @@
 """
 The Core class handles argument parsing using the argparse library.
 """
-import argparse
 import os
 
 from .loging.log import Log
+from .argparser import ArgParser
 
 class Core:
     """
@@ -12,50 +12,35 @@ class Core:
     """
 
     def __init__(self):
-        self.parser = argparse.ArgumentParser(
-            prog='PCE',
-            description='Python CLI Editor',
-            epilog='Use vozicode -help'
-        )
+        self.parser = ArgParser()
         self.log= Log()
-
-    def _create_arg_parser(self):
-        """
-        Creates an argument parser and adds the filename argument.
-        """
-        self.parser.add_argument('filename')
-
-    def parse_args(self):
-        """
-        Parses the command-line arguments using the argument parser.
-        """
-        try:
-            args = self.parser.parse_args()
-            return args
-        except SystemExit:
-            return None
-        except Exception as e:
-            self.log.error(f"Error: {e}")
-            return None
 
     def start(self):
         """
         Starts the Core class by creating the argument parser 
         and parsing the command-line arguments.
         """
-        self._create_arg_parser()
-        args = self.parse_args()
-        if args:
-            file_name = args.filename
 
-            try:
-                with open(os.path.join(os.getcwd(), file_name), 'r', encoding='utf-8') as file:
-                    file_text = file.read()
-                    os.system('cls' if os.name == 'nt' else 'clear')
-                    print(file_text, end="\r")
-            except FileNotFoundError:
-                self.log.error(f"Error: File '{file_name}' not found.")
-                return
-            except Exception as e:
-                self.log.error(f"Error: {e}")
-                return
+        file_name = self.parser.filename
+
+        if file_name:
+            self.print_file_content(file_name)
+
+
+    def print_file_content(self, file_name: str):
+        """cool
+
+        Args:
+            file_name (str): _description_
+        """
+        try:
+            with open(os.path.join(os.getcwd(), file_name), 'r', encoding='utf-8') as file:
+                file_text = file.read()
+                os.system("cls" if os.name == "nt" else "clear")
+                print(file_text, end="\r")
+        except FileNotFoundError:
+            self.log.error(f"Error: File '{file_name}' not found.")
+            return
+        except Exception as e:
+            self.log.error(f"Error: {e}")
+            return
